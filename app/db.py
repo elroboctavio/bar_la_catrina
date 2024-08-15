5fd8d1bcde38b88f5ff8eb431d871188951c3511
elroboctavio <elroboctavio@gmail.com>
2024-08-15 14:55:20 -0600

    Aumento del pool

--- app/db.py
+++ app/db.py
@@ -2,7 +2,7 @@ from psycopg2 import pool
 
 #Crear un pool de conexiones
 connection_pool = pool.SimpleConnectionPool(
-    1,20,
+    1,50,
     database="bar_la_catrina",
     user="postgres",
     password="tVE4QgrFP9rnEb",
